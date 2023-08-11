96640eaf186e4d5c15b854cd6d19d873ce94d8c5
Miguel <141414291+soulangelo@users.noreply.github.com>
2023-08-11 08:33:49 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+let cor;
+let posicaoHorizontal;
+let posicaoVertical;
+let posicaoHorizontal2;
+let posicaoVertical2;
+let circuloX //horizontal
+let circuloY //vertical
+
+function setup() {
+ 
+ 
+  createCanvas(600,600);
+  background("white");
+  cor = color(random(0, 255), random(0, 255), random(0, 255))
+posicaoHorizontal = 0;
+posicaoVertical = random(height);
+
+posicaoHorizontal2 = 0;
+posicaoVertical2 = random(height);
+
+ 
+}
+
+function draw() {
+  stroke(cor);
+  fill(cor)
+
+  circle(posicaoHorizontal, posicaoVertical, 50);
+circle(posicaoHorizontal2, posicaoVertical2, 50);
+ 
+posicaoHorizontal+= random(1, 4);
+posicaoVertical+= random(-3, 3);
+ 
+posicaoHorizontal2+= random(1, 4);
+posicaoVertical2+= random(-3, 3);
+ 
+ 
+ 
+ 
+  if(mouseIsPressed) {
+  cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+ 
+  }
+ 
+   
+  }
